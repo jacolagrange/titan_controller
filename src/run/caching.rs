d6bd5531562096bec8aaa9f839528a7eda141482
Jaime Roelandts <jaime.roelandts@ugent.be>
2025-07-09 17:39:39 +0200

    Removed print statement

--- src/run/caching.rs
+++ src/run/caching.rs
@@ -13,7 +13,7 @@ pub fn get_hash_sniper_config(exp_args: &str) -> u64 {
     let final_conf = p_args.get_final_config();
     //println!("Final_conf {:#?}", final_conf);
     let hash = hash_config_normalized(&final_conf);
-    println!("HASH is {hash}");
+    //println!("HASH is {hash}");
     hash
 }
 
